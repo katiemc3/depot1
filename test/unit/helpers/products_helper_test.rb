6ba3da3446517e968965ceab347c6990933eab69
Katie <k.mcmanamon4@nuigalway.ie>
2013-10-02 12:55:21 +0100

    mesages

--- test/unit/helpers/products_helper_test.rb
+++ test/unit/helpers/products_helper_test.rb
@@ -1,4 +1,27 @@
 require 'test_helper'
 
 class ProductsHelperTest < ActionView::TestCase
+
+def new_product(image_url)
+  Product.new(:title       => "My Book Title",
+              :description => "yyy",
+              :price       => 1,
+              :image_url   => image_url)
+end
+
+test "image url should have a certain extension" do
+  ok = ["fred.gif", "fred.jpg", "fred.png", "FRED.JPG", "FRED.Jpg",
+           "http://a.b.c/x/y/z/fred.gif"]
+  bad = ["fred.doc",  "fred.gif/more",  "fred.gif.more"]
+
+  ok.each do |name|
+    assert new_product(name).valid?, "#{name} shouldn't be invalid"
+  end
+
+  bad.each do |name|
+    assert new_product(name).invalid?, "#{name} shouldn't be valid"
+  end
+end
+
+
 end
